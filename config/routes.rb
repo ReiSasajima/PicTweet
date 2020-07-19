fc2a01584cb6d42bbdb8b19cb725ea4e98072411
EC2 Default User <ec2-user@ip-172-31-41-253.ap-northeast-1.compute.internal>
2020-07-19 15:03:10 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'tweets#index'
+    get 'tweets' => 'tweets#index'
+    # tweetscontrollerのdef indexに繋がる
+    get 'tweets/new' => 'tweets#new'
+    post 'tweets' => 'tweets#create'
+    
+    get 'users/:id' => 'users#show'
+    delete 'tweets/:id' => 'tweets#destroy'
+    get 'tweets/:id/edit' => 'tweets#edit'
+    
+    patch 'tweets/:id' => 'tweets#update'
+    
+    get 'tweets/:id' => 'tweets#show'
+end
